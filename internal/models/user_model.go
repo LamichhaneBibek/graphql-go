3268ff0bb9db253d90fc11bbb5f0b2088d551874
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2026-05-13 12:04:42 +0545

    Add User model to represent users with authentication details

---
+++ internal/models/user_model.go
@@ -0,0 +1,12 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"uniqueIndex;not null"`
+	Password string `gorm:"not null"`
+	RoleID   *uint
+	Role     *Role
+}
