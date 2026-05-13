c8a18006b1ec2371d86b98cdf32ed271aa7e14c0
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2026-05-13 12:02:51 +0545

    Refactor main.go to implement graceful server shutdown and improve signal handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,31 +1,60 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-
+	"github.com/LamichhaneBibek/graphql-go/config"
 	"github.com/LamichhaneBibek/graphql-go/graph"
+	"github.com/LamichhaneBibek/graphql-go/internal/models"
+	"github.com/LamichhaneBibek/graphql-go/internal/repository"
+	"github.com/LamichhaneBibek/graphql-go/internal/seed"
+	"github.com/LamichhaneBibek/graphql-go/internal/service"
 )
 
-const defaultPort = "8000"
-
 func main() {
+	cfg := config.Load()
+	db := config.ConnectDB(cfg)
 
-	srv := handler.NewDefaultServer(
-		graph.NewExecutableSchema(
-			graph.Config{
-				Resolvers: &graph.Resolver{},
-			},
-		),
+	db.AutoMigrate(
+		&models.Permission{},
+		&models.Role{},
+		&models.User{},
+		&models.Post{},
 	)
 
-	http.Handle("/", playground.Handler("GraphQL Playground", "/query"))
-	http.Handle("/query", srv)
+	seed.Roles(db)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	userRepo := repository.NewUserRepository(db)
+	postRepo := repository.NewPostRepository(db)
+	roleRepo := repository.NewRoleRepository(db)
 
-	log.Fatal(http.ListenAndServe(":"+defaultPort, nil))
+	resolver := &graph.Resolver{
+		AuthService: service.NewAuthService(userRepo),
+		UserService: service.NewUserService(userRepo, roleRepo),
+		PostService: service.NewPostService(postRepo),
+	}
+
+	srv := newServer(resolver, cfg)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("server listening on :%s", cfg.Port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	<-quit
+	log.Println("shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	srv.Shutdown(ctx)
 }
