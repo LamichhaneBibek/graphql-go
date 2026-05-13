24d518304a9f896fffc9604ec81a7aef3f2b20eb
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2026-05-13 12:03:36 +0545

    Add services to Resolver for dependency injection in GraphQL API

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,13 @@
 package graph
 
+import "github.com/LamichhaneBibek/graphql-go/internal/service"
+
 // This file will not be regenerated automatically.
 //
-// It serves as dependency injection for your app, add any dependencies you require
-// here.
+// It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	AuthService service.AuthService
+	UserService service.UserService
+	PostService service.PostService
+}
